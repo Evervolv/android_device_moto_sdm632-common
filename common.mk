f5f7e85755afc3fc63293b941ca65154e92da637
William Bellavance <flintman@flintmancomputers.com>
2022-01-17 18:36:37 -0500

    Add in android.frameworks.sensorservice@1.0.vendor
    <MCI><ERROR> mm_camera_load_shim_lib: 3357: dlopen failed with error dlopen failed: library "android.frameworks.sensorservice@1.0.so" not found: needed by /vendor/lib/libmmcamera2_stats_modules.so in namespace (default)
    
    Change-Id: I2fe6787973afe9a246ea3568d51495252d9b95ba

--- common.mk
+++ common.mk
@@ -433,7 +433,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl:64 \
     android.hardware.sensors@1.0-service \
-    android.hardware.sensors@1.0.vendor
+    android.hardware.sensors@1.0.vendor \
+    android.frameworks.sensorservice@1.0.vendor
 
 # Shims
 PRODUCT_PACKAGES += \
