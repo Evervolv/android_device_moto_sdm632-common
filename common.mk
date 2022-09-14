43a377821a4aeeacef178939e6f26aec6f7c747d
Bruno Martins <bgcngm@gmail.com>
2022-09-14 14:51:15 +0100

    sdm632-common: Migrate to AIDL ClearKey DRM HAL
    Change-Id: Iad5cc1176978137c647069197b33f49fb4d98ab2

--- common.mk
+++ common.mk
@@ -164,7 +164,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm-service.clearkey
 
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
