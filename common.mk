14387b9f50c49955034e21ebb60ff3bc39169071
William Bellavance <flintman@flintmancomputers.com>
2022-11-01 17:36:25 -0400

    Revert "sdm632-common: Import custom livedisplay"
    No longer needed
    
    This reverts commit 93e6996c02fdca400b75d22ac42e088dd5f2a134.

--- common.mk
+++ common.mk
@@ -279,8 +279,8 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm.motorola_sdm632 \
-    vendor.lineage.livedisplay@2.0-service-sysfs.motorola_sdm632
+    vendor.lineage.livedisplay@2.0-service-sdm \
+    vendor.lineage.livedisplay@2.0-service-sysfs
 
 # MotoActions
 PRODUCT_PACKAGES += \
